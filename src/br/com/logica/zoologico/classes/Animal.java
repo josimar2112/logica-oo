1c35abe05c68df1e750a26da7376992b9c10a34e
Josimar Bruno Benvenutti <68384@proway.treina>
2020-09-27 16:48:05 -0300

    atribuindo valores as proprieda

--- src/br/com/logica/zoologico/classes/Animal.java
+++ src/br/com/logica/zoologico/classes/Animal.java
@@ -4,9 +4,34 @@ package br.com.logica.zoologico.classes;
 public class Animal {
 
 	// atributos ou propriedades do objeto
-	//neste momento as propriedades são publicas
-	String nome;
-	String especie;
-	int idade;
+	// neste momento as propriedades são publicas
+	private String nome;
+	private String especie;
+	private int idade;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getespecie() {
+		return especie;
+	}
+
+	public int getIdade() {
+		return idade;
+
+	}
 
 }
--- src/br/com/logica/zoologico/principal/Main.java
+++ src/br/com/logica/zoologico/principal/Main.java
@@ -1,5 +1,26 @@
 package br.com.logica.zoologico.principal;
 
+import br.com.logica.zoologico.classes.Animal;
+
 public class Main {
 
+	public static void main(String[] args) {
+		//criando uma nova instancia do objeto
+		Animal a1 = new Animal();
+
+		a1.setNome ("Jogo todinho");
+		a1.especie = "Gato";
+		a1.idade = 1;
+		
+		// criando um nova instancia do objeto
+		Animal a2 = new Animal();
+		
+		//atribuindo os valores as propriedades.
+		a2.setNome ("Spike");
+		a2.especie = "cachorro";
+		a2.idade = 3;
+		
+		System.out.println("O nome do meu anil é " + a2.getNome());
+			}
+	
 }
