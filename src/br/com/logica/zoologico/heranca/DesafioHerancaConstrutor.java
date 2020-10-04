22812580ca936775c0aa11362176622dda9cdac8
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-10-04 18:54:03 -0300

    Desafios 2

---
+++ src/br/com/logica/zoologico/heranca/DesafioHerancaConstrutor.java
@@ -0,0 +1,71 @@
+package br.com.logica.zoologico.heranca;
+
+import java.util.Scanner;
+class PessoaDesafio {
+    private String nome;
+    private int idade;
+
+    public PessoaDesafio(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void showInfo() {
+        System.out.println(nome);
+        System.out.println(idade);
+    }
+}
+
+class ProgramadorDesafio extends PessoaDesafio{
+
+    private String linguagem;
+
+    public ProgramadorDesafio(String nome, int idade) {
+        super(nome, idade);
+    }
+
+    public ProgramadorDesafio(String nome, int idade, String linguagem) {
+        super(nome, idade);
+        this.linguagem = linguagem;
+    }
+
+    @Override
+    public void showInfo() {
+        super.showInfo();
+        System.out.println(linguagem);
+    }
+}
+
+public class DesafioHerancaConstrutor {
+
+    /*
+
+    Informe abaixo uma classe chamada Programador, que respeite as regras abaixo:
+    Contenha uma propriedade privada linguagem textual;
+    A proprieade deve ser iniciada no construtor da classe;
+    Sobrescreva o método showInfo() da superclasse;
+    Exiba os dados do "Programador".
+
+    Entrada de dados
+    Carlos Silva
+    33
+    Java
+
+    Saída esperada
+    Carlos Silva
+    33
+    Java
+
+    Dica: Utilize a cláusula super para acessar membros da classe herdada.
+     */
+
+
+    public static void main(String []args){
+        Scanner scann = new Scanner(System.in);
+
+        PessoaDesafio a = new ProgramadorDesafio(scann.nextLine(), Integer.valueOf(scann.nextLine()), scann.nextLine());
+
+        a.showInfo();
+    }
+}
+
---
+++ src/br/com/logica/zoologico/heranca/DesafioSobrescritaMetodo.java
@@ -0,0 +1,46 @@
+package br.com.logica.zoologico.heranca;
+
+import java.util.Scanner;
+
+class AnimalDesafio {
+	private String nome;
+	private String especie;
+
+	public void setNome(String nome) {
+		this.nome = nome;
+
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setEspecie(String especie) {
+		this.especie = especie;
+	}
+
+	public String getEspecie() {
+		return especie;
+	}
+}
+
+class GatoDesafio extends AnimalDesafio {
+	@Override
+	public String getEspecie() {
+		return getNome() + " é um gato ";
+	}
+}
+
+public class DesafioSobrescritaMetodo {
+
+	public static void main(String[] args) {
+		AnimalDesafio a = new GatoDesafio();
+
+		Scanner scann = new Scanner(System.in);
+		a.setNome(scann.nextLine());
+
+		System.out.println(a.getEspecie());
+
+	}
+
+}
