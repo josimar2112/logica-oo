576c03f0fdd55d7f61ba12b1a00e930193777df2
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-10-10 17:02:05 -0300

    criando uma tabela

--- src/br/com/logica/zoologico/scrip-banco.sql
+++ src/br/com/logica/zoologico/scrip-banco.sql
@@ -1,3 +1,14 @@
 -- criar schema ou banco
 
 CREATE SCHEMA `zoo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci ;
+
+-- script para criar uma tabela
+
+CREATE TABLE `zoo`.`animal` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `especie` VARCHAR(20) NOT NULL,
+  `idade` INT NULL,
+  `estavivo` TINYINT NULL,
+  PRIMARY KEY (`codigo`));
+
+  
