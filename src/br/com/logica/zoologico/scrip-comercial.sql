ef94a4646f514ef9a65718c4965c11e7763ba539
Josimar Bruno Benvenutti <68384@PROWAY.TREINA>
2020-10-18 17:25:02 -0300

    criando constraints no banco

--- src/br/com/logica/zoologico/scrip-comercial.sql
+++ src/br/com/logica/zoologico/scrip-comercial.sql
@@ -107,6 +107,37 @@ insert into cliente (codigo_cliente , nome , razao_social, data_cadastro , cnpj 
     
     -- fim da criação dos index --
     
+       -- Inicio da criação das constraints de chave estrangeira --
+alter table venda add constraint fk_produto_fornecedor
+foreign key(id_fornecedor)
+references fornecedor(id)
+on delete no action
+on update no action;
+
+alter table venda add constraint fk_venda_cliente
+foreign key(id_cliente)
+references cliente(id)
+on delete no action
+on update no action;
+
+alter table venda add constraint fk_venda_vendedor
+foreign key(id_vendedor)
+references vendedor(id)
+on delete no action
+on update no action;
+
+alter table venda add constraint fk_venda_fornecedor
+foreign key(id_fornecedor )
+references fornecedor(id)
+on delete no action
+on update no action;
+
+alter table item_venda add constraint fk_item_venda_produto
+foreign key(id_produto)
+references produto (id)
+on delete no action
+on update no action;
+--  Fim da criação das constraints --
     
     
     
